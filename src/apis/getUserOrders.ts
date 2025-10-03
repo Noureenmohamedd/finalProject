9563a73262fe5eb206881e1ba30edb2322282415
[noureenmohamedd] <[noureenmohamed000@gmail.com]>
2025-10-03 03:10:04 +0300

    e

--- src/apis/getUserOrders.ts
+++ src/apis/getUserOrders.ts
@@ -1,21 +1,30 @@
 "use server"
 
+
+
 import getMyToken from "@/utilities/token"
 import axios from "axios"
 import { jwtDecode } from "jwt-decode"
 
-export default async function getUserOrder(){
-    const token = await getMyToken()
+export default async function getUserOrder() {
+  const token = await getMyToken()
 
-    const {id} = jwtDecode(token) as { id: string }
+  
+  if (!token || typeof token !== "string") {
+    throw new Error("Login first")
+  }
 
-    if(!token){
-        throw new Error ("Login first")
+
+  const { id } = jwtDecode<{ id: string }>(token)
+
+  const { data } = await axios.get(
+    `https://ecommerce.routemisr.com/api/v1/orders/user/${id}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`, 
+      },
     }
+  )
 
-    const data = await axios.get(`https://ecommerce.routemisr.com/api/v1/orders/user/${id}`)
-
-
-    return data
-
+  return data
 }
